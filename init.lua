63d5afc9eafa1f764e0c098925bc1679ab1b6121
Max Lie <max.lie@trepos.com>
2023-11-19 20:36:33 +0100

    yank in registers

--- init.lua
+++ init.lua
@@ -123,6 +123,22 @@ if has ("syntax")
 	syntax on
 endif
 
+" With each yank store in register
+function! SaveLastReg()
+    if v:event['regname']==""
+        if v:event['operator']=='y'
+            for i in range(8,1,-1)
+                exe "let @".string(i+1)." = @". string(i) 
+            endfor
+            if exists("g:last_yank")
+                let @1=g:last_yank
+            endif
+            let g:last_yank=@"
+        endif 
+    endif
+endfunction 
+autocmd TextYankPost * call SaveLastReg()
+
 " Mouse settings
 " set clipboard+=unnamedplus
 set clipboard=unnamedplus
