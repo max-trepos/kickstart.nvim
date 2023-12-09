016ad8d277aa9c7039169377df0280e2a4df0e52
Max Lie <max.lie@trepos.com>
2023-12-09 12:21:26 +0100

    markdown added

--- init.lua
+++ init.lua
@@ -812,7 +812,11 @@ vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'help', 'vim', 'html', 'json', 'javascript', 'sql'},
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx'
+                      , 'typescript', 'help', 'vim', 'html', 'json', 'javascript'
+                      , 'sql'
+                      , 'markdown', 'markdown_inline'
+                     },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
