847f298e29803fb6be58f130863a1fbbd9f7611c
Max Lie <max.lie@trepos.com>
2025-08-06 17:41:09 +0200

    checkin

--- init.lua
+++ init.lua
@@ -592,7 +592,51 @@ require('lazy').setup({
   {'voldikss/vim-floaterm'},
   {'moll/vim-bbye'},
   {'skywind3000/asyncrun.vim'},
-   
+  
+---@type LazySpec
+{
+  "mikavilpas/yazi.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    { "nvim-lua/plenary.nvim", lazy = true },
+  },
+  keys = {
+    -- 👇 in this section, choose your own keymappings!
+    {
+      "<leader>-",
+      mode = { "n", "v" },
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
+    },
+    {
+      -- Open in the current working directory
+      "<leader>fe",
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open the file manager in nvim's working directory",
+    },
+    {
+      "<c-up>",
+      "<cmd>Yazi toggle<cr>",
+      desc = "Resume the last yazi session",
+    },
+  },
+  ---@type YaziConfig | {}
+  opts = {
+    -- if you want to open yazi instead of netrw, see below for more info
+    open_for_directories = false,
+    keymaps = {
+      show_help = "<f1>",
+    },
+  },
+  -- 👇 if you use `open_for_directories=true`, this is recommended
+  init = function()
+    -- mark netrw as loaded so it's not loaded at all.
+    --
+    -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+    vim.g.loaded_netrwPlugin = 1
+  end,
+},
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
